c19f1049bc6b33be8541c65f371b6b1dcf99b4d2
Rosie Babbra <rosie.babbra@nielsen.com>
2020-01-20 00:53:21 -0800

    renaming weird vars

--- music_brand_mentions/retrieve_brand_mentions.py
+++ music_brand_mentions/retrieve_brand_mentions.py
@@ -4,16 +4,33 @@ import json
 from utilities import retrieve_saturday_dates
 
 
-def _count_brands(text, substr):
+def _count_substring_occurance(text, substr):
     """Loop through every file in 'data/lyrics' to count 
-    the occurances of each brand in each file."""
+    the occurances of each brand in each file.
+    
+    Args:
+        text (str): file from which to search substrings
+        substr (str): desired substring to be found
+
+    Returns:
+        int: Number of occurances of substring in text document.
+    
+    """
 
     return text.count(substr)
 
 
-def read_lyrics_file(file):
+def count_brand_occurances(file_path):
+    """
+    Args:
+        file_path (str): location of file to be read in
 
-    text = open(file).read()
+    Returns:
+        dict: Occurances per brand.
+    
+    """
+
+    text = open(file_path).read()
 
     with open('data/brands.json') as brand_file:
         brands = json.load(brand_file)['brands']
@@ -21,7 +38,7 @@ def read_lyrics_file(file):
     brand_ct = {}
 
     for brand in brands:
-        brand_ct[brand] = _count_brands(text, brand)
+        brand_ct[brand] = _count_substring_occurance(text, brand)
 
     return brand_ct
 
@@ -33,4 +50,4 @@ if __name__ == '__main__':
     dates = [date.strftime("%Y-%m-%d") for date in retrieve_saturday_dates(2019)]
 
     for date in dates:
-        brand_cts[date] = read_lyrics_file(f'data/lyrics/{date}_lyrics.txt')
+        brand_cts[date] = count_brand_occurances(f'data/lyrics/{date}_lyrics.txt')
